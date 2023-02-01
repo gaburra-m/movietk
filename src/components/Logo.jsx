fe71906ed17948fc5309f93a43f084e0c91da644
gaburra-m <gaburra.m@gmail.com>
2023-02-01 15:06:12 -0600

    Corrigiendo url logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,8 +4,8 @@ import logo from "../logo.svg";
 function Logo() {
 return (
     <div className="logo">
-        <a href="/"><img src={logo} className="logo__img" alt="logo movieTK" href="./" /></a>
-        <a href="/" className="logo__txt"><h1 >movieTK</h1></a>
+        <a href="/movietk/"><img src={logo} className="logo__img" alt="logo movieTK" /></a>
+        <a href="/movietk/" className="logo__txt"><h1 >movieTK</h1></a>
         </div>
 )
 }
